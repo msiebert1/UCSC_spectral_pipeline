fcd2058dcf7f2a957635cfa4c7677466afb1d2bb
Jonathan Brown <brojonat@gmail.com>
2018-09-26 14:39:20 -0700

    added kastbias to util.py. Made some changes to pre_reduction.py that should make things a little clearer. Changed tabs to spaces, added checks for iraf existing files, and added calls to kastbias. Also rearranged to top of the file with the option parser and how the main() is executed, but mostly superficial.

--- spectral_reduction/util.py
+++ spectral_reduction/util.py
@@ -2,6 +2,131 @@ from __future__    import print_function
 try:      from astropy.io import fits as pyfits
 except:   import pyfits
 
+#---------------------------------------------------------------------------
+# 
+# kastbias -- perform overscan bias subtraction from KAST images
+#
+# Inputs:
+#   ifile -- input file to perform overscan bias subtration
+#   ofile -- output file
+#
+# Returns:
+#   0, but writes to ofile
+#
+# Description:
+#   Performs KAST bias subtraction.
+#   Adapted from Elinor Gates routine. 
+#
+# Author:
+#   J. Brown, UCSC Astronomy
+#   brojonat@ucsc.edu
+#   2018 Sep 26
+#
+# Version 2.1 -- Elinor Gates, 2016 Aug 25 - changed math to BITPIX -32
+# Version 2.0 -- Elinor Gates, 2016 Aug 11 - improved file reading
+# Version 1.0 -- Elinor Gates, 2015 Nov 24
+def kastbias(ifile,ofile):
+    
+    data, header = fits.getdata(ifile,header=True)
+
+    # change data to float
+    data=data.astype('float32')
+
+    # read necessary keywords from fits header
+    xsize = header['NAXIS1']
+    ysize = header['NAXIS2']
+    xorig = header['CRVAL1U']
+    yorig = header['CRVAL2U']
+    cdelt1 = header['CDELT1U']
+    cdelt2 = header['CDELT2U']
+    rover = header['ROVER']
+    cover = header['COVER']
+    inxsize = header['DNAXIS1']
+    inysize = header['DNAXIS2']
+    ampsx = header['AMPSCOL']
+    ampsy = header['AMPSROW']
+
+    # determine number and sizes of overscan and data regions
+    namps = ampsx*ampsy
+    if rover > 0:
+        over=rover
+        sys.exit('Program does not yet deal with row overscans. Exiting.')
+    else:
+        over = cover
+    if over == 0:
+        sys.exit('No overscan region specified in FITS header. Exiting.')
+
+    # single amplifier mode
+    if namps == 1:
+        biassec = data[:,xsize-cover:xsize]
+        datasec = data[0:,0:xsize-cover]
+
+        # median overscan section
+        bias=np.median(biassec, axis=1) 
+        # subtract overscan
+        datanew = datasec
+        for i in range(datasec.shape[1]):
+            datanew[:,i] = datasec[:,i]-bias
+
+    # two amplifier mode
+    if namps == 2:
+        biasseca = data[:,xsize-cover*2:xsize-cover]
+        biassecb = data[:,xsize-cover:xsize]
+
+        # median overscan sections
+        biasa=np.median(biasseca,axis=1)
+        biasb=np.median(biassecb,axis=1)
+
+        # extract data regions
+
+        #determine size of binned data region
+        hsize=abs(inxsize/cdelt1)
+
+        # calculate x origin of readout in binned units if cdelt1 negative or positive
+        if cdelt1 < 0:
+            xorig=(xorig-(xsize-2*cover)*abs(cdelt1))/abs(cdelt1)
+        else:
+            xorig=xorig/cdelt1
+        x0=xorig+xsize-1-cover*2 # need to test is need -1 because starting counting at 0
+
+        # determine which columns are on which amplifier and subtract proper overscan region
+
+        if x0 < hsize/2: # all data on left amplifier
+            datanew=data[:,0:xsize-cover*2]
+            m=datanew.shape[1]
+            for i in range(0,m):
+                datanew[:,i]=datanew[:,i]-biasa
+
+        if xorig >= hsize/2: # all data on right amplifier
+            datanew=data[:,0:xsize-cover*2]
+            m=datanew.shape[1]
+            for i in range(0,m):
+                datanew[:,i]=datanew[:,i]-biasb
+
+        if xorig < hsize/2 and x0 > hsize/2:
+            x1=hsize/2-xorig
+            dataa=data[:,0:x1]
+            datab=data[:,x1:-cover*2]
+            ma=dataa.shape[1]
+            mb=datab.shape[1]
+            for i in range(0,ma):
+                dataa[:,i]=dataa[:,i]-biasa
+            for i in range(0,mb):
+                datab[:,i]=datab[:,i]-biasb
+            # merge dataa and datab into single image
+            datanew=np.hstack([dataa,datab])
+        
+    if namps > 2: 
+        sys.exit('Program does not yet deal with more than two overscan regions. Exiting.')
+
+    # add info to header
+    header['HISTORY'] = 'Overscan subtracted'
+
+    # write new fits file
+    fits.writeto(ofile,datanew,header,clobber=True)
+
+    return 0
+
 def ReadAscii2(ascifile):
     # print "LOGX:: Entering `ReadAscii2` method/function in %(__file__)s" %
     # globals()
