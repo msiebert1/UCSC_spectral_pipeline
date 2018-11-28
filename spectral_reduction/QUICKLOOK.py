80db36dbc565f5fc8ad939986b6e95372d1d4614
Jonathan Brown <brojonat@gmail.com>
2018-11-28 12:14:33 -0800

    substantial changes to pre_reduced and removed some reduntant blocks of code, but still needs some work, removed tabs in all files, cleaned up the trunk, added instrument cal directories, made changes to instruments.py including pointing the cal files to the correct locations.

--- spectral_reduction/QUICKLOOK.py
+++ spectral_reduction/QUICKLOOK.py
@@ -11,78 +11,81 @@ import matplotlib
 matplotlib.use('TkAgg')
 
 description = "> Fast reduction of spectra "
-usage = "%prog	\t [option] \n Recommended syntax: %prog -i -c"
+usage = "%prog    \t [option] \n Recommended syntax: %prog -i -c"
 
 if __name__ == "__main__":
-	parser = OptionParser(usage=usage, description=description, version="0.1" )
-	#parser.add_option("-d", "--dispersion", dest="dispersion",
-	#				  action="store_true",help='chose interactively dispersion line')
-	#parser.add_option("-t", "--trace", dest="trace",
-	#				  action="store_true", help=' trace extraction with another frame')
-	#parser.add_option("-s", "--sens", dest="sens",
-	#				  default='', type="str", help=' use sensitivity curve from this list')
-	#parser.add_option("-a", "--arc", dest="arc",
-	#				  default='', type="str", help=' use arc from this list ')
-	#parser.add_option("-r", "--check_reidentify_arc", dest="check_reidentify_arc",
-	#				  action="store_true",help='check the reidentification')
-	#parser.add_option("-o", "--overlapping_region", dest="overlapping_region",
-	#				  action="store_true",help='check overlapping region')
-	parser.add_option("-a", "--arc", dest="arc", action="store_true",
-					  help='identify arcs of the same night')
-	parser.add_option("-i", "--interactive_extraction", dest="interactive_extraction",
-					  action="store_true",help='extract spectrum interactively')
-	parser.add_option("-c", "--cosmic", dest="cosmic",
-					  action="store_true",help='cosmic ray removal')
+    parser = OptionParser(usage=usage, description=description, version="0.1" )
+    #parser.add_option("-d", "--dispersion", dest="dispersion",
+    #                  action="store_true",help='chose interactively dispersion line')
+    #parser.add_option("-t", "--trace", dest="trace",
+    #                  action="store_true", help=' trace extraction with another frame')
+    #parser.add_option("-s", "--sens", dest="sens",
+    #                  default='', type="str", help=' use sensitivity curve from this list')
+    #parser.add_option("-a", "--arc", dest="arc",
+    #                  default='', type="str", help=' use arc from this list ')
+    #parser.add_option("-r", "--check_reidentify_arc", dest="check_reidentify_arc",
+    #                  action="store_true",help='check the reidentification')
+    #parser.add_option("-o", "--overlapping_region", dest="overlapping_region",
+    #                  action="store_true",help='check overlapping region')
+    parser.add_option("-a", "--arc", dest="arc", action="store_true",
+                      help='identify arcs of the same night')
+    parser.add_option("-i", "--interactive_extraction", dest="interactive_extraction",
+                      action="store_true",help='extract spectrum interactively')
+    parser.add_option("-c", "--cosmic", dest="cosmic",
+                      action="store_true",help='cosmic ray removal')
 
-	option, args = parser.parse_args()
+    option, args = parser.parse_args()
 
-	starttime = time.time()
+    starttime = time.time()
 
-	util.delete('*.png')
-	_arc= option.arc
+    util.delete('*.png')
+    _arc= option.arc
 
-	if len(args) > 1:
-		files=[]
-		sys.argv.append('--help')
-		option, args = parser.parse_args()
-	elif len(args) == 1:
-		files = util.readlist(args[0])
-	else:
-		listfile = glob.glob('t*.fits')
-		files_science = []
-		files_arc = []
-		files_flat = []
-		# prep_files = glob.glob('../*.fits')
-		files_arc.append('../ARC_blue.fits')
-		files_arc.append('../ARC_red.fits')
-		#print 'checking your files ...'
-		for img in listfile:
-			_type = ''
-			hdr0 = util.readhdr(img)
-			_type=util.readkey3(hdr0, 'object')
-			if _type.startswith("arc"):
-				files_arc.append(img)
-			elif 'RESP' in img:
-				files_flat.append(img)
-			else:
-				files_science.append(img)
+    if len(args) > 1:
+        files=[]
+        sys.argv.append('--help')
+        option, args = parser.parse_args()
+    elif len(args) == 1:
+        files = util.readlist(args[0])
+    else:
+        listfile = glob.glob('t*.fits')
+        files_science = []
+        files_arc = []
+        files_flat = []
+        # prep_files = glob.glob('../*.fits')
+        files_arc.append('../ARC_blue.fits')
+        files_arc.append('../ARC_red.fits')
+        #print 'checking your files ...'
+        for img in listfile:
+            _type = ''
+            hdr0 = util.readhdr(img)
+            _type=util.readkey3(hdr0, 'object')
+            if _type.startswith("arc"):
+                files_arc.append(img)
+            elif 'RESP' in img:
+                files_flat.append(img)
+            else:
+                files_science.append(img)
 
-	_cosmic = option.cosmic
-	if not _cosmic:	  _cosmic = False
-	
-	_interactive_extraction = option.interactive_extraction
-	if not _interactive_extraction:	  _interactive_extraction = False
+    _cosmic = option.cosmic
+    if not _cosmic:
+        _cosmic = False
+    
+    _interactive_extraction = option.interactive_extraction
+    if not _interactive_extraction:
+        _interactive_extraction = False
 
-	
-	if not _arc:   _arc = False
+    
+    if not _arc:
+        _arc = False
 
-	if len(files_science) > 0:
-		print('\n#######################################\n### start of reduction')
-		outputfile = quick_reduc.reduce(files_science, files_arc, files_flat, _cosmic, _interactive_extraction,_arc)
-		stoptime = time.time()
-		print('\n### wow, only ' + str(stoptime - starttime) + ' seconds')
-		print('\n### end of reduction')
-		print('\n#######################################')
-	else:
-		print('\n### problem, exit ')
-		sys.exit()
+    if len(files_science) > 0:
+        print('\n#######################################\n### start of reduction')
+        outputfile = quick_reduc.reduce(files_science, files_arc, files_flat, _cosmic, _interactive_extraction,_arc)
+        stoptime = time.time()
+        print('\n### wow, only ' + str(stoptime - starttime) + ' seconds')
+        print('\n### end of reduction')
+        print('\n#######################################')
+    else:
+        print('\n### problem, exit ')
+        sys.exit()
--- spectral_reduction/instruments.py
+++ spectral_reduction/instruments.py
@@ -1,6 +1,7 @@
 from __future__    import print_function
 import os
 import util
+
 path_to_trunk = os.path.expandvars('$UCSC_SPECPIPE/spectral_reduction/trunk/')
 if not os.path.exists(path_to_trunk):
     raise RuntimeError('Error : $UCSC_SPECPIPE variable is undefined or incorrect!')
@@ -16,22 +17,22 @@ kast_blue = {'name': 'kast_blue',
              'biassec': '[1:2048,318:350]',
              'trimsec': '[50:2010,7:287]',
              # 'trimsec': '[30:2000,5:285]', YC
-             'archive_zero_file': path_to_trunk + 'Zero_blue_20180206.fits',
+             'archive_zero_file': path_to_trunk + 'KAST_cals/Zero_blue_20180206.fits',
              # 'archive_flat_file': path_to_trunk + 'Resp_blue_20180206.fits', THIS FLAT IS GARBAGE
-             'archive_flat_file': path_to_trunk + 'RESP_blue.fits',
+             'archive_flat_file': path_to_trunk + 'KAST_cals/RESP_blue.fits',
              # 'archive_sens': path_to_trunk + 'sens_function_blue_20180206.fits',
-             'archive_sens': path_to_trunk + 'fluxstarblue.fits',
-             'archive_bstar': path_to_trunk + 'bstarblue.fits',
+             'archive_sens': path_to_trunk + 'KAST_cals/fluxstarblue.fits',
+             'archive_bstar': path_to_trunk + 'KAST_cals/bstarblue.fits',
              # 'archive_arc_extracted': path_to_trunk + 'tbb1070.ms.fits',
              # 'archive_arc_extracted_id': path_to_trunk + 'id/idtbb1070.ms',
-             'archive_arc_extracted': path_to_trunk + 'Blue_Arc_Ref.ms.fits',
-             'archive_arc_extracted_id': path_to_trunk + 'id/idBlue_Arc_Ref.ms',
-             'archive_arc_aperture': path_to_trunk + 'apBlue_Arc_Ref',
+             'archive_arc_extracted': path_to_trunk + 'KAST_cals/Blue_Arc_Ref.ms.fits',
+             'archive_arc_extracted_id': path_to_trunk + 'KAST_cals/id/idBlue_Arc_Ref.ms',
+             'archive_arc_aperture': path_to_trunk + 'KAST_cals/apBlue_Arc_Ref',
              # 'line_list': path_to_trunk+'Lines_He_Hg_Cd_HgA_Ne_KAST.dat',
-             'line_list': path_to_trunk+'lines.dat',
-             'extinction_file': path_to_trunk + 'lick_extinction.dat',
+             'line_list': path_to_trunk+'KAST_cals/lines.dat',
+             'extinction_file': path_to_trunk + 'KAST_cals/lick_extinction.dat',
              'observatory': 'lick',
-             'sky_file': path_to_trunk + 'licksky.fits',
+             'sky_file': path_to_trunk + 'KAST_cals/licksky.fits',
              'section': 'middle line'}
 
 
@@ -46,22 +47,22 @@ kast_red = { 'name': 'kast_red',
              'biassec': '[360:405,1:2725]',
              'trimsec': '[66:346,110:2500]',
              # 'trimsec': '[55:355,95:2500]', YC
-             'archive_zero_file': path_to_trunk + 'Zero_red_20180206.fits',
+             'archive_zero_file': path_to_trunk + 'KAST_cals/Zero_red_20180206.fits',
              # 'archive_flat_file': path_to_trunk + 'Resp_red_20180206.fits',
-             'archive_flat_file': path_to_trunk + 'RESP_red.fits',
+             'archive_flat_file': path_to_trunk + 'KAST_cals/RESP_red.fits',
              # 'archive_sens': path_to_trunk + 'sens_function_red_20180206.fits',
-             'archive_sens': path_to_trunk + 'fluxstarred.fits',
-             'archive_bstar': path_to_trunk + 'bstarred.fits',
+             'archive_sens': path_to_trunk + 'KAST_cals/fluxstarred.fits',
+             'archive_bstar': path_to_trunk + 'KAST_cals/bstarred.fits',
              # 'archive_arc_extracted': path_to_trunk + 'tbr1103.ms.fits',
              # 'archive_arc_extracted_id': path_to_trunk + 'id/idtbr1103.ms',
-             'archive_arc_extracted': path_to_trunk + 'Red_Arc_Ref.ms.fits',
-             'archive_arc_extracted_id': path_to_trunk + 'id/idRed_Arc_Ref.ms',
-             'archive_arc_aperture': path_to_trunk + 'apRed_Arc_Ref',
+             'archive_arc_extracted': path_to_trunk + 'KAST_cals/Red_Arc_Ref.ms.fits',
+             'archive_arc_extracted_id': path_to_trunk + 'KAST_cals/id/idRed_Arc_Ref.ms',
+             'archive_arc_aperture': path_to_trunk + 'KAST_cals/apRed_Arc_Ref',
              # 'line_list': path_to_trunk+'Lines_He_Hg_Cd_HgA_Ne_KAST.dat',
-             'line_list': path_to_trunk+'lines.dat',
-             'extinction_file': path_to_trunk + 'lick_extinction.dat',
+             'line_list': path_to_trunk+'KAST_cals/lines.dat',
+             'extinction_file': path_to_trunk + 'KAST_cals/lick_extinction.dat',
              'observatory': 'lick',
-             'sky_file': path_to_trunk + 'licksky.fits',
+             'sky_file': path_to_trunk + 'KAST_cals/licksky.fits',
              'section': 'middle column'}
 
 lris_blue = {'name': 'lris_blue',
@@ -72,50 +73,32 @@ lris_blue = {'name': 'lris_blue',
              'filter': 'temp',
              'slit': 'temp',
              'dispaxis': 1,
-             'biassec': '[1:2048,318:350]',
-             'trimsec': '[50:2010,7:287]',
-             # 'trimsec': '[30:2000,5:285]', YC
-             'archive_zero_file': path_to_trunk + 'Zero_blue_20180206.fits',
-             # 'archive_flat_file': path_to_trunk + 'Resp_blue_20180206.fits', THIS FLAT IS GARBAGE
-             'archive_flat_file': path_to_trunk + 'RESP_blue.fits',
-             # 'archive_sens': path_to_trunk + 'sens_function_blue_20180206.fits',
-             'archive_sens': path_to_trunk + 'fluxstarblue.fits',
-             'archive_bstar': path_to_trunk + 'bstarblue.fits',
-             # 'archive_arc_extracted': path_to_trunk + 'tbb1070.ms.fits',
-             # 'archive_arc_extracted_id': path_to_trunk + 'id/idtbb1070.ms',
-             'archive_arc_extracted': path_to_trunk + 'LRIS_Blue_Arc_Ref.ms.fits',
-             'archive_arc_extracted_id': path_to_trunk + 'id/idLRIS_Blue_Arc_Ref.ms',
-             'archive_arc_aperture': path_to_trunk + 'apLRIS_Blue_Arc_Ref',
-             # 'line_list': path_to_trunk+'Lines_He_Hg_Cd_HgA_Ne_KAST.dat',
-             'line_list': path_to_trunk+'LRIS_lines.dat',
-             'extinction_file': path_to_trunk + 'keck_extinction.dat',
+             'archive_flat_file': path_to_trunk + 'LRIS_cals/RESP_blue.fits',
+             'archive_sens': path_to_trunk + 'LRIS_cals/fluxstarblue.fits',
+             'archive_bstar': path_to_trunk + 'LRIS_cals/bstarblue.fits',
+             'archive_arc_extracted': path_to_trunk + 'LRIS_cals/LRIS_Blue_Arc_Ref.ms.fits',
+             'archive_arc_extracted_id': path_to_trunk + 'LRIS_cals/id/idLRIS_Blue_Arc_Ref.ms',
+             'archive_arc_aperture': path_to_trunk + 'LRIS_cals/apLRIS_Blue_Arc_Ref',
+             'line_list': path_to_trunk+'LRIS_cals/LRIS_lines.dat',
+             'extinction_file': path_to_trunk + 'LRIS_cals/lick_extinction.dat', # JB: need to fix
              'observatory': 'keck',
-             'sky_file': path_to_trunk + 'kecksky.fits',
+             'sky_file': path_to_trunk + 'LRIS_cals/kecksky.fits',
              'section': 'middle line'}
              
 lris_red = { 'name': 'lris_red',
              'arm': 'red',
              'read_noise': 4.7,
              'gain': 1.2,
-             'biassec': '[360:405,1:2725]',
-             'trimsec': '[66:346,110:2500]',
-             # 'trimsec': '[55:355,95:2500]', YC
-             'archive_zero_file': path_to_trunk + 'Zero_red_20180206.fits',
-             # 'archive_flat_file': path_to_trunk + 'Resp_red_20180206.fits',
-             'archive_flat_file': path_to_trunk + 'RESP_red.fits',
-             # 'archive_sens': path_to_trunk + 'sens_function_red_20180206.fits',
-             'archive_sens': path_to_trunk + 'fluxstarred.fits',
-             'archive_bstar': path_to_trunk + 'bstarred.fits',
-             # 'archive_arc_extracted': path_to_trunk + 'tbr1103.ms.fits',
-             # 'archive_arc_extracted_id': path_to_trunk + 'id/idtbr1103.ms',
-             'archive_arc_extracted': path_to_trunk + 'LRIS_Red_Arc_Ref.ms.fits',
-             'archive_arc_extracted_id': path_to_trunk + 'id/idLRIS_Red_Arc_Ref.ms',
-             'archive_arc_aperture': path_to_trunk + 'apLRIS_Red_Arc_Ref',
-             # 'line_list': path_to_trunk+'Lines_He_Hg_Cd_HgA_Ne_KAST.dat',
-             'line_list': path_to_trunk+'LRIS_lines.dat',
-             'extinction_file': path_to_trunk + 'keck_extinction.dat',
+             'archive_flat_file': path_to_trunk + 'LRIS_cals/RESP_red.fits',
+             'archive_sens': path_to_trunk + 'LRIS_cals/fluxstarred.fits',
+             'archive_bstar': path_to_trunk + 'LRIS_cals/bstarred.fits',
+             'archive_arc_extracted': path_to_trunk + 'LRIS_cals/LRIS_Red_Arc_Ref.ms.fits',
+             'archive_arc_extracted_id': path_to_trunk + 'LRIS_cals/id/idLRIS_Red_Arc_Ref.ms',
+             'archive_arc_aperture': path_to_trunk + 'LRIS_cals/apLRIS_Red_Arc_Ref',
+             'line_list': path_to_trunk+'LRIS_cals/LRIS_lines.dat',
+             'extinction_file': path_to_trunk + 'LRIS_cals/lick_extinction.dat', # JB: need to fix
              'observatory': 'keck',
-             'sky_file': path_to_trunk + 'kecksky.fits',
+             'sky_file': path_to_trunk + 'LRIS_cals/kecksky.fits',
              'section': 'middle column'}
              
 goodman_m1={ 'name': 'goodman_blue',
@@ -143,10 +126,10 @@ goodman_m1={ 'name': 'goodman_blue',
              'archive_arc_extracted_id': path_to_trunk + 'SOAR_cals/id/idm1_Arc_Ref.ms',
              'archive_arc_aperture': path_to_trunk + 'SOAR_cals/apm1_Arc_Ref',
              # 'line_list': path_to_trunk+'Lines_He_Hg_Cd_HgA_Ne_KAST.dat',
-             'line_list': path_to_trunk+'lines.dat',
-             'extinction_file': path_to_trunk + 'lick_extinction.dat',
+             'line_list': path_to_trunk+'SOAR_cals/lines.dat',
+             'extinction_file': path_to_trunk + 'lick_extinction.dat', # JB: need to fix
              'observatory': 'soar',
-             'sky_file': path_to_trunk + 'licksky.fits',
+             'sky_file': path_to_trunk + 'licksky.fits', # JB: need to fix
              'section': 'middle column'}
              
 goodman_m2={ 'name': 'goodman_red',
@@ -173,10 +156,10 @@ goodman_m2={ 'name': 'goodman_red',
              'archive_arc_extracted_id': path_to_trunk + 'SOAR_cals/id/idm2_Arc_Ref.ms',
              'archive_arc_aperture': path_to_trunk + 'SOAR_cals/apm2_Arc_Ref',
              # 'line_list': path_to_trunk+'Lines_He_Hg_Cd_HgA_Ne_KAST.dat',
-             'line_list': path_to_trunk+'lines.dat',
-             'extinction_file': path_to_trunk + 'lick_extinction.dat',
+             'line_list': path_to_trunk+'SOAR_cals/lines.dat',
+             'extinction_file': path_to_trunk + 'SOAR_cals/lick_extinction.dat', # JB: need to fix
              'observatory': 'soar',
-             'sky_file': path_to_trunk + 'licksky.fits',
+             'sky_file': path_to_trunk + 'SOAR_cals/licksky.fits', # JB: need to fix
              'section': 'middle column'}
 
 
